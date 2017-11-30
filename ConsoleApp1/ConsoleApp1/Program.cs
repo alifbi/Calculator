6f23b7ec8c7c7201983fe74bbe6aa8241878a75e
aliaalipour <ali.aalipour.72@gmail.com>
2017-11-30 12:21:38 +0330

    fir commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter Num 1: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Num 2: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int sum = a + b;
+            int sub = a - b;
+
+            Console.WriteLine("Sum Is :" + sum);
+            Console.WriteLine("sub Is :" + sub);
+            
+            Console.ReadLine();
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Properties/AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GitProjectCalculator")]
+[assembly: AssemblyTitle("ConsoleApp1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GitProjectCalculator")]
+[assembly: AssemblyProduct("ConsoleApp1")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3ecfb5de-4e92-45ab-a750-78fa80c8e477")]
+[assembly: Guid("d5facda7-3038-433d-a5ac-6546fed5eea3")]
 
 // Version information for an assembly consists of the following four values:
 //
